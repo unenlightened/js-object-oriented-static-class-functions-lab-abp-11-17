7f4da7f7284f12a1f64ff77fdc5a31e558fa0245
Chely Ho <adept_inept@yahoo.com>
2017-11-10 01:32:39 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,15 +1,13 @@
 'use strict';
 
 class Question {
-  save(){
-    User._All.push(this)
-  }
-
   static All(){
     return this._All;
   }
 
-
+  save(){
+    Question._All.push(this)
+  }
 }
 
 Question._All = [];
