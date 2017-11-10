3ad765fc74eb014137e4670457d68b54092ce231
Chely Ho <adept_inept@yahoo.com>
2017-11-10 01:50:10 +0000

    Done.

--- question.js
+++ question.js
@@ -4,6 +4,19 @@ class Question {
   static All(){
     return this._All;
   }
+
+  save(){
+    Question._All.push(this);
+  }
+
+  constructor(content){
+    this.content = content;
+    this.save();
+  }
+
+  static Find(id){
+    return Question._All[id-1];
+  }
 }
 
 Question._All = [];
